c3e9e7e5c6262eece58df25d31bd68b92a2ce3eb
CallieSardina <callie.sardina@bc.edu>
2024-02-26 12:23:18 -0800

    update

--- parse_logs.py
+++ parse_logs.py
@@ -78,39 +78,47 @@ for game_idx in list(final_result.keys()):
             equation3 = equations[2]
             equation4 = equations[3]
             prompt = """You were previously given the following instructions: 
-                Use numbers and basic arithmetic operations (+ - * /) to obtain 24. 
+                Use numbers and basic integer arithmetic operations (+ - * /) to obtain 24. You may only use three equations to reach the answer, and you should never get an intermediate decimal value. 
+                The output of the first equation is used with as an input in the next equation along with another of the input numbers. This pattern continues for all three equations.
                 At each subsequent step, you were prompted to evaluate if given numbers can reach 24 (sure/likely/impossible).
                 At the final step, you were given an input and an answer and instructed to give a judgement (sure/impossible) if the answer is correct, 
                 i.e. it uses each input exactly once and no other numbers, and reach 24.
-
-                Given the numbers """ + str(tmp_x) + """, your initial output was that it is impossible to reach 24. 
+            
+                Input: Given the numbers """ + str(x) + """, your initial output was that it is impossible to reach 24. 
                 Let\'s explore the thought process and identify where the error occurred. Break down your solution into individual steps, 
                 explaining the reasoning behind each operation.
 
                 Tree of Thought:
 
-                Start by reviewing the initial input """ + str(tmp_x) + """.
+                Start by reviewing the initial input """ + str(x) + """.
                 The first operation you used was """ + str(equation1) + """ 
                 Explain the first operation performed and its purpose. 
                 That is, explain why you chose these numbers and corresponding operation to start with as opposed to any other combination.
                 If an error occurred in the first step, identify the correct operation that should have been chosen.
                 Continue this process step by step until the incorrect output is reached.
-                The second operation you used was """ + str(equation2) +  """.
+                The second operation you used was """ + str(equation2) + """.
                 The third operation you used was """ + str(equation3) + """.
-                The fourth operation you used was """ + str(equation4) + """. 
-                Start from the fourth step, and determine if you could change this step to reach a correct solutions resulting in 24.
-                If not, repeat this analysis for the third step, second step, and first step as necessry.
+                You provided a fourth step (which is nto permitted). It was """ + str(equation4) + """.
+                Start from the final step, and determine if you could change this step to reach a correct solutions resulting in 24.
+                The correct final step is: """ + solution_prompts.gen_solution_prompt(game_idx) + """
+                Given this information, reevaluate, then repeat this analysis for the second step and first step as necessry.
                 For each step, provide the correct operation and the correct subsequent operations and result.
-                Conclude by summarizing the correct sequence of operations that lead to the correct solution for the game of 24.
-                
-                Evaluate whether your new response correctly evaluates to 24. If it does not, review the following: """ + "\n" + solution_prompts.gen_solution_prompt(game_idx)
+                Conclude by summarizing the correct sequence of operations that lead to the correct solution for the game of 24, and report the solution
+                in the following format, remember that you can only use 3 equations to reach 24::
+                [Equation 1]: 
+                [Equation 2]:
+                [Equation 3]: 
+
+                Evaluate whether your new response correctly evaluates to 24.""" 
+                # If it does not, review the following: """ + "\n" + solution_prompts.gen_solution_prompt(game_idx)
 
         elif len(equations) == 3:
             equation1 = equations[0]
             equation2 = equations[1]
             equation3 = equations[2]
             prompt = """You were previously given the following instructions: 
-                Use numbers and basic arithmetic operations (+ - * /) to obtain 24. 
+                Use numbers and basic integer arithmetic operations (+ - * /) to obtain 24. You may only use three equations to reach the answer, and you should never get an intermediate decimal value. 
+                The output of the first equation is used with as an input in the next equation along with another of the input numbers. This pattern continues for all three equations.
                 At each subsequent step, you were prompted to evaluate if given numbers can reach 24 (sure/likely/impossible).
                 At the final step, you were given an input and an answer and instructed to give a judgement (sure/impossible) if the answer is correct, 
                 i.e. it uses each input exactly once and no other numbers, and reach 24.
@@ -131,13 +139,19 @@ for game_idx in list(final_result.keys()):
                 The third operation you used was """ + str(equation3) + """.
                 After the third operation, you deemed it impossible to reach 24 with the given input numbers.
                 Start from the third step, and determine if you could change this step to reach a correct solutions resulting in 24.
-                If not, repeat this analysis for the second step and first step as necessry.
+                The correct third step is: """ + solution_prompts.gen_solution_prompt(game_idx) + """
+                Given this information, reevaluate, then repeat this analysis for the second step and first step as necessry.
                 For each step, provide the correct operation and the correct subsequent operations and result.
-                Conclude by summarizing the correct sequence of operations that lead to the correct solution for the game of 24.
+                Conclude by summarizing the correct sequence of operations that lead to the correct solution for the game of 24, and report the solution
+                in the following format, remember that you can only use 3 equations to reach 24::
+                [Equation 1]: 
+                [Equation 2]:
+                [Equation 3]: 
 
-                Evaluate whether your new response correctly evaluates to 24. If it does not, review the following: """ + "\n" + solution_prompts.gen_solution_prompt(game_idx)
+                Evaluate whether your new response correctly evaluates to 24.""" 
+                # If it does not, review the following: """ + "\n" + solution_prompts.gen_solution_prompt(game_idx)
 
-        filepath = "./new_prompts/prompt_" + str(game_idx) + ".txt"
+        filepath = "./given_eq3/prompt_" + str(game_idx) + ".txt"
         f = open(filepath, "w")
         f.write(prompt)
         f.close()
--- solution_prompts.py
+++ solution_prompts.py
@@ -6,18 +6,22 @@ def gen_solution_prompt(index):
         f = os.path.join(directory, filename)
         if os.path.isfile(f) and filename[7: 10] == str(index):
             with open(f, 'r') as fn:
-                prompt = """For reference in the analysis of where the error occurred, 
-                        I have provided the correct sequence of operations to reach 24 is listed below. 
-                        If there are multiple possible solution paths, these are delimited by '***'.
-                        Please use this information to determine where you made your previous mistake, 
-                        and report why you made the error you did. Explore properties of the numbers and equations,
-                        why your decision making process was faulty, and how it could be improved. 
-                        Report your new answer in the form: 
-                        [Equation 1]
-                        [Equation 2]
-                        [Equation 3]
-                        Furthermore, please use this information to generate a general strategy for solving the game of 24.""" + "\n" + fn.read()
+                equation_3 = fn.read().split("***")[0].split("\n")[2]
+                prompt = equation_3 + """. Use this information to reason about how to reach this final conclusion, and determine the 
+                first two steps in solving this game of 24."""
+                # prompt = """For reference in the analysis of where the error occurred, 
+                        # I have provided the correct sequence of operations to reach 24 is listed below. 
+                        # If there are multiple possible solution paths, these are delimited by '***'.
+                        # Please use this information to determine where you made your previous mistake, 
+                        # and report why you made the error you did. Explore properties of the numbers and equations,
+                        # why your decision making process was faulty, and how it could be improved. 
+                        # Report your new answer in the form: 
+                        # [Equation 1]
+                        # [Equation 2]
+                        # [Equation 3].""" + "\n" + fn.read()
     return prompt
+
+
 
 
 
